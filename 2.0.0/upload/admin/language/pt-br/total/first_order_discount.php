4e486a48cc129d5a120201f2fdeffc6fc5c6f6a1
luizsene <luiz@pentaxial.com.br>
2017-05-24 16:18:59 -0300

    Coreção tradução

---
+++ 2.0.0/upload/admin/language/pt-br/total/first_order_discount.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']           		= 'Primeira Compra de desconto';
+
+$_['tab_general']      		        = 'Configurações';
+$_['tab_help']        		        = 'Ajudar';
+
+// Text
+$_['text_module']            		= 'Modules';
+$_['text_success']           		= 'Sucesso: Módulo Você modificou Primeira Compra de desconto!';
+$_['text_discount_fixed']           = 'fixo';
+$_['text_discount_percent']         = 'Porcentagem';
+
+// Entry
+$_['entry_status']                  = 'Status:';
+$_['entry_min_subtotal_amount']     = 'Quantidade Min SubTotal';
+$_['entry_min_subtotal_amount_help']     = 'Subtotal (sem impostos ou outros descontos) necessária antes de o desconto fica disponível<br />Deixe em branco se min subtotal não é necessária';
+$_['entry_customer_groups']         = 'Grupos de clientes:';
+$_['entry_customer_groups_help']         = 'deixe em branco = aplicar a todos os grupos';
+$_['entry_order_statuses']          = 'Contagem pedidos de clientes com status:';
+$_['entry_order_statuses_help']          = 'ex: Complete<br />contar apenas os pedidos com status selecionado';
+$_['entry_check_ip']                = 'Check IP:';
+$_['entry_check_ip_help']                = 'irá verificar se existe uma ordem de IP atual do cliente. <br />Se não for encontrada uma ordem do mesmo ip = desconto em não permitidos';
+$_['entry_sort_order']              = 'Ordem de classificação:';
+$_['entry_discount_type']     		= 'tipo de desconto';
+$_['entry_discount_amount']   		= 'valor do desconto';
+
+// Error
+$_['error_permission']       		= 'Atenção: Você não tem permissão para modificar lealdade módulo vida de desconto!';
+$_['error_allowed_statuses']        = 'Erro: Por favor escolha status para ordens válidas!';
+$_['error_discount_amount']         = 'Erro: valor de desconto necessário!';
+
+?>
